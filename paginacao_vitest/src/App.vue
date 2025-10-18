78e95a761ae871862348195f6a191b899560ec93
vhstn <victorhugosotnas2@gmail.com>
2025-10-18 19:27:34 -0300

    chore: Configurações iniciais

---
+++ paginacao_vitest/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-3xl font-bold text-green-600 p-4 border rounded shadow">
+    Hello Tailwind!
+  </h1>
+</template>
+
+<style></style>
